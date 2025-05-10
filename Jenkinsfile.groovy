d93b385618fbd97c7ac977e9ed7e0d98b36f6293
huangdi33 <8641538+huangdi33@user.noreply.gitee.com>
2025-05-11 00:45:48 +0800

    Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,10 +2,8 @@ pipeline {
     agent { label 'docker-capable' }
 
     environment {
-        // 设置 Docker 和 kubectl 的路径，根据你的Jenkins环境调整
         PATH = "/usr/local/bin:${env.PATH}"
         DOCKER_IMAGE = "test/001-springboot-demo-helloworld:${env.BUILD_NUMBER}"
-        //KUBECONFIG = credentials('kubeconfig') // 使用你在Jenkins中保存的KubeConfig凭据ID
     }
 
     stages {
@@ -13,7 +11,7 @@ pipeline {
             steps {
                 git branch: 'master',
                         url: 'https://github.com/huangmo2017/spring-demos.git',
-                        credentialsId: 'github-token'  // 替换为你在Jenkins中配置的凭据ID
+                        credentialsId: 'github-token'
             }
         }
 
@@ -25,11 +23,17 @@ pipeline {
             }
         }
 
+        stage('Debug - Check Files') {
+            steps {
+                sh 'ls -la'
+                sh 'ls -la ./target/'
+                sh 'cat ./001-springboot-demo-helloworld/Dockerfile'
+            }
+        }
+
         stage('Build Docker Image') {
-            agent { label 'docker-capable' }
             steps {
                 script {
-                    // 指定 Dockerfile 的路径和构建上下文路径
                     docker.build(DOCKER_IMAGE, "-f ./001-springboot-demo-helloworld/Dockerfile .")
                 }
             }
